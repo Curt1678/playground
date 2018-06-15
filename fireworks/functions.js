b219e931fe58e13867e4ee27bdb2e887edc4acae
Curtis Sheen <curtis@paliltd.com>
2018-06-15 16:25:51 +0100

    added night sky (used window.onload from /planets)

--- fireworks/functions.js
+++ fireworks/functions.js
@@ -1,3 +1,18 @@
+window.onload = function(){
+	for(i=0; i<200; i++){
+		let windowWidth = window.innerWidth;
+		let windowHeight = window.innerHeight;
+		let starX = Math.random() * 98;
+		let starY = Math.random() * 98;
+		let star = document.createElement('div');
+		
+		star.classList.add('star');
+		star.style = "left: " + starX.toString() + "%; top: " + starY.toString() + "%;";
+		
+		document.querySelector('body').append(star);		
+	}
+};
+
 let shrapnelAmount = 100;	// set number of firework projectiles here
 
 window.addEventListener('click',function(e){
